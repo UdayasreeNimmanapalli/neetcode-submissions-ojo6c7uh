59f9e9dcfd34ca6f23fa2f7941675f7337b7e6fc
Udayasree Nimmanapalli <udayasreenimmanapalli@gmail.com>
2026-04-01 22:30:18 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.go
@@ -0,0 +1,15 @@
+func dailyTemperatures(temperatures []int) []int {
+	var res = make([]int,len(temperatures))
+	for i:=0;i<len(temperatures);i++{
+		res[i] = 0
+	}
+	var stack = make([]int,0)
+	for i:=0;i<len(temperatures);i++{
+		for len(stack)>0 && temperatures[i]>temperatures[stack[len(stack)-1]]{
+			res[stack[len(stack)-1]] = i-stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		} 
+		stack = append(stack, i)
+	}
+	return res
+}
