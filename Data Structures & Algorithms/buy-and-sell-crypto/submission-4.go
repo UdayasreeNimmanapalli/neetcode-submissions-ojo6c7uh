0d15bad76ab9e64dc76085602a4e2448a628060e
Udayasree Nimmanapalli <udayasreenimmanapalli@gmail.com>
2026-05-04 14:57:04 +0530

    Update: buy-and-sell-crypto - submission-4

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-4.go
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.go
@@ -1,14 +1,11 @@
 func maxProfit(prices []int) int {
-	var buy = prices[0]
+	buy:=prices[0]
 	var maxProfit = 0
 	for i:=1;i<len(prices);i++{
-		if prices[i]<buy{
+		if buy>prices[i]{
 			buy = prices[i]
 		}else{
-			profit := prices[i]-buy
-			if profit>maxProfit{
-				maxProfit = profit
-			}
+			maxProfit = max(maxProfit, prices[i]-buy)
 		}
 	}
 	return maxProfit
